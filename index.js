30585f5778be563560724348ea78699432031440
eddie dunsford <eddie.dunsford@gmail.com>
2024-10-16 13:03:02 -0400

    add files

---
+++ index.js
@@ -0,0 +1,23 @@
+// Prompt the user to enter a list of froyo flavors separated by commas
+const userInput = prompt("Enter a list of froyo flavors separated by commas", "vanilla,vanilla,vanilla,strawberry,coffee,coffee");
+
+// Convert the user input into an array of flavors
+const flavorArray = userInput.split(',')
+
+// Create an empty object to store flavor counts
+const flavorCount = {};
+
+// Loop through the flavor array
+for (let i = 0; i < flavorArray.length; i++) {
+  const flavor = flavorArray[i];
+}
+ 
+
+// Display the flavor count object in the console
+//console.log("Your froyo flavor counts:", flavorCount);
+
+
+
+
+
+
